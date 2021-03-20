a181bd214c1246a4f9069effa44dd1f09e4bd9a7
Thiago Rodrigues <thiagorodrigues1203@outlook.com>
2021-03-20 16:49:34 -0300

    six

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiBaseUrl: 'http://localhost:4040/api/contatos'
-  //apiBaseUrl: 'https://agenda-api-new.herokuapp.com/api/contatos'
+  apiBaseUrl: 'https://agenda-api-new.herokuapp.com/api/contatos'
+  //apiBaseUrl: 'http://localhost:4040/api/contatos'
   
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,8 +4,7 @@
 
 export const environment = {
   production: false,
-  //apiBaseUrl: '/api/contatos'
-  apiBaseUrl: 'http://localhost:4040/api/contatos'
+  //apiBaseUrl: 'http://localhost:4040/api/contatos'
 };
 
 /*
